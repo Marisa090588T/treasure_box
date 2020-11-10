8ba42092196ef01b527ba50692be2656fce2dc27
Marisa Tsubokura <marieee090530@gmail.com>
2020-11-10 20:41:53 +0900

    navbar and dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,5 @@
 class DashboardsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
     @item_listings = ItemListing.where(user: current_user)
     @stores = Store.where(user: current_user)
   end
